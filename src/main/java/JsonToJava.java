02bb219d42fa60e59c797f5a8a9a7d5e78882dc4
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2023-02-10 12:34:08 -0300

    10. Converting Java objects to Json file using Jackson API

--- src/main/java/JsonToJava.java
+++ src/main/java/JsonToJava.java
@@ -1,20 +1,31 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
 public class JsonToJava {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn;
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business", "root", "root");
-
+        CustomerInfo c = new CustomerInfo();
         //object of statement class will help us to excecute queries
         Statement st = conn.createStatement();
         ResultSet rs = st.executeQuery("select * from CustomerInfo where purchasedDate=CURDATE() and Location ='Asia';");
         while (rs.next()) {
-            System.out.println(rs.getString(1));
-            System.out.println(rs.getString(2));
-            System.out.println(rs.getInt(3));
-            System.out.println(rs.getString(4));
+            c.setCourseName(rs.getString(1));
+            c.setPurchasedDate(rs.getString(2));
+            c.setAmount(rs.getInt(3));
+            c.setLocation(rs.getString(4));
+
+            System.out.println(c.getCourseName());
+            System.out.println(c.getAmount());
+            System.out.println(c.getLocation());
+            System.out.println(c.getPurchasedDate());
         }
+        ObjectMapper o = new ObjectMapper();
+        o.writeValue(new File("C:\\Users\\ruehlein\\git\\JsonJava\\customerInfo.json"), c);
         conn.close();
     }
 }
