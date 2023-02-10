1d08bbda5068c4d457248c6ecfb1f084045f20d5
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2023-02-10 14:11:10 -0300

    14. Apache common String Utilities to Parse strings with escape characters

--- src/main/java/JsonToJava.java
+++ src/main/java/JsonToJava.java
@@ -1,9 +1,15 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.apache.commons.text.StringEscapeUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class JsonToJava {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
@@ -13,6 +19,7 @@ public class JsonToJava {
 
         ObjectMapper o = new ObjectMapper();
         ArrayList<CustomerInfo> a = new ArrayList<>();
+        JSONArray ja = new JSONArray();
 
         //object of statement class will help us to excecute queries
         Statement st = conn.createStatement();
@@ -32,8 +39,21 @@ public class JsonToJava {
             System.out.println(c.getPurchasedDate());
         }
 
-        for (int i = 0; i < a.size(); i++) {
-            o.writeValue(new File("C:\\Users\\ruehlein\\git\\JsonJava\\customerInfo" + i + ".json"), a.get(i));
+        IntStream.range(0, a.size()).forEach(i -> {
+            String jsonString = new Gson().toJson(a.get(i));
+            ja.add(jsonString);
+        });
+
+        JSONObject jo = new JSONObject();
+        jo.put("data", ja);
+        String json = StringEscapeUtils.unescapeJava(jo.toString());
+        json = json
+                .replace("\"{", "{")
+                .replace("}\"", "}");
+        System.out.println(json);
+        o.writeValue(new File("C:\\Users\\ruehlein\\git\\JsonJava\\customerInfo.json"), json);
+        try (FileWriter file = new FileWriter("C:\\Users\\ruehlein\\git\\JsonJava\\customerInfoSingle.json")) {
+            file.write(json);
         }
 
         conn.close();
